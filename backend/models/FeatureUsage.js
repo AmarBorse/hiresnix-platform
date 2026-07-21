81d22c36399a92f68d2ccbb2940d6b3f9c104101
Amar Borse <amar.borse24@pcu.edu.in>
2026-07-21 15:22:44 +0530

    fix: sequelize destructure import in models

--- backend/models/FeatureUsage.js
+++ backend/models/FeatureUsage.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+const { sequelize } = require('../config/db');
 
 const FeatureUsage = sequelize.define('FeatureUsage', {
   id:        { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
